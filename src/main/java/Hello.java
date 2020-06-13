85ba244429cbbd3ac241d9a0d205965cb3b3aa2b
moa1128 <moa971128@gmail.com>
2020-06-14 04:45:23 +0900

    commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,9 @@
+package main.java;
+
 public class Hello {
     public static void main(String args[]){
         System.out.println("Hello World");
         System.out.println("hyori-test");
+        System.out.println("jieun-test");
     }
 }
