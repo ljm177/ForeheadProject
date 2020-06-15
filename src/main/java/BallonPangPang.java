6703ecede73d8299c2c0c16efd73b879caa94fba
jeongmin <jmlee2232@gmail.com>
2020-06-15 17:23:28 +0900

    Login 생성자 : CardLayout 로그인창

--- src/main/java/BallonPangPang.java
+++ src/main/java/BallonPangPang.java
@@ -22,6 +22,36 @@ class Login extends JFrame implements ActionListener {
 
     public Login() {
 
+        setTitle("::풍선이 팡팡:");
+
+        card = new CardLayout();
+        setLayout(card); // 카드레이아웃으로 설정
+        loginP = new JPanel();
+
+        add(loginP, "loginP");
+
+        p1 = new JPanel(new BorderLayout());
+        loginP.add(p1);
+        p2 = new JPanel(new GridLayout(1, 1));
+        p3 = new JPanel(new GridLayout(1, 1));
+        p1.add(p2, "West");
+        p1.add(p3, "Center");
+        p2.add(lbName = new JLabel("이   름  :  "));
+        p3.add(tfName = new TextField(20));
+
+        Panel p4 = new Panel(); // 버튼 부착
+        p1.add(p4, "South");
+        p4.add(btLogin = new JButton("Login"));
+        p4.add(btReset = new JButton("Reset"));
+        p4.add(btExit = new JButton("Exit"));
+
+        // 버튼 이벤트 리스너 등록
+        btLogin.addActionListener(this);
+        btReset.addActionListener(this);
+        btExit.addActionListener(this);
+
+        setSize(300, 130);
+        setVisible(true);
     }
 
     @Override
