a646c799cacb2acad9c2fd76754d41ad491dc0d7
jeongmin <jmlee2232@gmail.com>
2020-06-15 17:22:12 +0900

    BallonPangPang 멤버필드 재선언

--- src/main/java/BallonPangPang.java
+++ src/main/java/BallonPangPang.java
@@ -7,18 +7,18 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class BallonPangPang {
-    JPanel loginP, mainP;
-    JPanel p1, p2, p3;
-    CardLayout card;
-    JLabel lbName;
-    TextField tfName;
-    JButton btLogin, btReset, btExit;
 
     public static void main(String[] args) {
         new Login();
     }
 }
 class Login extends JFrame implements ActionListener {
+    JPanel loginP, mainP;
+    JPanel p1, p2, p3;
+    CardLayout card;
+    JLabel lbName;
+    TextField tfName;
+    JButton btLogin, btReset, btExit;
 
     public Login() {
 
