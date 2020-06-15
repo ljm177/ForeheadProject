1f0bff5afc9ec1fcfcb380c84cf9c3e8fa56990b
jeongmin <jmlee2232@gmail.com>
2020-06-15 17:21:41 +0900

    BallonPangPang 멤버필드 선언

--- src/main/java/BallonPangPang.java
+++ src/main/java/BallonPangPang.java
@@ -7,6 +7,13 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class BallonPangPang {
+    JPanel loginP, mainP;
+    JPanel p1, p2, p3;
+    CardLayout card;
+    JLabel lbName;
+    TextField tfName;
+    JButton btLogin, btReset, btExit;
+
     public static void main(String[] args) {
         new Login();
     }
